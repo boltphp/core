3101b7a797104d5e31e60b1b68a78102e3cc88cf
Travis <travis@teamcoco.com>
2014-04-11 16:27:53 +0000

    collection fix

--- src/bolt/helpers/collection.php
+++ src/bolt/helpers/collection.php
@@ -6,80 +6,79 @@ use \Closure;
 
 class collection implements \ArrayAccess, \Countable, \IteratorAggregate, \JsonSerializable {
 
-    private $_items = [];
-
+    protected $items = [];
 
     public function each(Closure $cb, $data = []) {
-        foreach ($this->_items as $key => $item) {
+        foreach ($this->items as $key => $item) {
             call_user_func($cb, $item, $key, $data);
         }
         return $this;
     }
 
     public function first() {
-        return count($this) > 0 ? reset($this->_items) : null;
+        return count($this) > 0 ? reset($this->items) : null;
     }
 
     public function last() {
-        return count($this) > 0 ? end($this->_items) : null;
+        return count($this) > 0 ? end($this->items) : null;
     }
 
     public function filter(Closure $cb) {
-        $this->_items = array_filter($this->_items, $cb);
+        $this->items = array_filter($this->items, $cb);
         return $this;
     }
 
     public function splice($offset, $length, $replace = []) {
-        array_splice($this->_items, $offset, $length, $replace);
+        array_splice($this->items, $offset, $length, $replace);
         return $this;
     }
 
     public function push($item) {
-        $this->_items[] = $item;
+        $this->items[] = $item;
         return $this;
     }
 
     public function shift() {
-        array_shift($this->_items);
+        array_shift($this->items);
         return $this;
     }
 
     public function unshift($item) {
-        array_unshift($this->_items, $item);
+        array_unshift($this->items, $item);
         return $this;
     }
 
     public function getIterator() {
-        return new \ArrayIterator($this->_items);
+        return new \ArrayIterator($this->items);
     }
 
     public function count() {
-        return count($this->_items);
+        return count($this->items);
     }
 
     public function jsonSerialize() {
-        return json_encode($this->_items);
+        return json_encode($this->items);
     }
 
     public function offsetSet($offset, $value) {
         if (is_null($offset)) {
-            $this->_items[] = $value;
+            $this->items[] = $value;
         }
         else {
-            $this->_items[$offset] = $value;
+            $this->items[$offset] = $value;
         }
     }
 
     public function offsetExists($offset) {
-        return isset($this->_items[$offset]);
+        return isset($this->items[$offset]);
     }
 
     public function offsetUnset($offset) {
-        unset($this->_items[$offset]);
+        unset($this->items[$offset]);
     }
 
     public function offsetGet($offset) {
-        return isset($this->_items[$offset]) ? $this->_items[$offset] : null;
+        return isset($this->items[$offset]) ? $this->items[$offset] : null;
     }
 
 
