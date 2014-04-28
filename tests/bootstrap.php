3b5ceb807bfc5474aadde5613b2d7ae3a39f49e0
Travis <travis@teamcoco.com>
2014-04-28 17:08:20 +0000

    fixed test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,13 @@
 <?php
 
-// require_once __DIR__."/../vendor/autoload.php";
-// require_once __DIR__."/../src/bolt.php";
-
-
-require __DIR__.'/../bolt.phar';
+require_once __DIR__."/../vendor/autoload.php";
+require_once __DIR__."/../src/bolt.php";
 
 date_default_timezone_set('UTC');
 
 define("VENDOR_DIR", realpath(__DIR__."/../vendor/"));
 define("MOCK_DIR", realpath(__DIR__."/mock"));
+
 define("TEST_ROOT", realpath(__DIR__));
 
 error_reporting(E_ALL);
